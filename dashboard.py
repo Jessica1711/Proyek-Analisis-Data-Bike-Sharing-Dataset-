f89fe210874e6568c8d8e25973a650978b5ad284
Jessica1711 <6162101157@student.unpar.ac.id>
2025-03-10 00:15:50 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,16 @@
+!pip install streamlit babel
+import streamlit as st
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
 sns.set(style='dark')
+
+from google.colab import drive
+drive.mount('/content/drive')
+day_df = pd.read_csv('/content/drive/MyDrive/Proyek Analisis Data/day.csv', delimiter=",")
+day_df.head()
+hour_df = pd.read_csv('/content/drive/MyDrive/Proyek Analisis Data/hour.csv', delimiter=",")
+hour_df.head()
